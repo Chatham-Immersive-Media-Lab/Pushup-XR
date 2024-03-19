2e416209580c6b8a336f67a115409e1ebef90f1d
Disuko <64217407+disukomusic@users.noreply.github.com>
2024-03-19 16:46:20 -0400

    pushup counter

--- Assets/PushupCounter.cs
+++ Assets/PushupCounter.cs
@@ -1,20 +1,65 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PushupCounter : MonoBehaviour
 {
-    private int pushups;
-    private Collider previousCollider;
+    public GameObject planePrefab;
+    public GameObject head;
+    public float downPos;
+    public float upPos;
+    private bool atDownPosition = false;
+    private bool atUpPosition = false;
+    private bool positionsSet;
 
-    private void OnTriggerEnter(Collider other)
+    public int pushupCount;
+    private void Start()
     {
-        if (other != previousCollider)
+        StartCoroutine(GetPositions());
+    }
+
+    private void Update()
+    {
+        if (positionsSet)
         {
-            pushups++;
-            previousCollider = other;
-            Debug.Log(pushups);
+            if (!atDownPosition && head.transform.position.y <= downPos + 0.05f)
+            {
+                atDownPosition = true;
+            }
+            if (!atUpPosition && head.transform.position.y >= upPos - 0.05f)
+            {
+                atUpPosition = true;
+            }
+            if (atDownPosition && atUpPosition)
+            {
+                pushupCount++;
+                pushupCount /= 2; //BAD DUMB STUPID BAD
+                Debug.Log("Pushup");
+                atDownPosition = false;
+                atUpPosition = false;
+            }
         }
+    }
+
+    IEnumerator GetPositions()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Press space to set low position");
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        downPos = head.transform.position.y;
+
+        Instantiate(planePrefab, head.transform.position, quaternion.identity);
+            
+        yield return new WaitForSeconds(1f);
+   
+        Debug.Log("Press space to set high position");
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        upPos = head.transform.position.y;
+        Instantiate(planePrefab, head.transform.position, quaternion.identity);
+        
+        yield return new WaitForSeconds(1f);
+
+        positionsSet = true;
+        Debug.Log("Positions set successfully");
     }
 }
